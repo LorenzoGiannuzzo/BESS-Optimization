14f17f34df32d91b78c84d4d0ee476daed9057e7
LorenzoGiannuzzo <lorenzo.giannuzzo@polito.it>
2024-06-05 10:24:46 +0200

    Update #3

--- BESS Trading.py
+++ BESS Trading.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import ExcelOpener
-import pybamm
 import matplotlib
 
 class BESS_Trader:
@@ -10,7 +9,15 @@ class BESS_Trader:
 
    file_path = "BESS Data.xlsx"
    sheetname = "BESS Properties"
+   sheetname2 = "Li-ion ChargeDischarge Curve"
    Properties = ExcelOpener.import_file.load_excel(file_path, sheetname)
+   load_curve = ExcelOpener.import_file.load_excel(file_path, sheetname2)
+
+
+
+
+
+
 
 
 

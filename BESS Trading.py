317047ec4ffb40dd5227ca78827c5d486b2ad07f
LorenzoGiannuzzo <lorenzo.giannuzzo@polito.it>
2024-06-05 12:28:07 +0200

    Update #4

--- BESS Trading.py
+++ BESS Trading.py
@@ -1,19 +1,95 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import  matplotlib
+import os
 import ExcelOpener
-import matplotlib
+import Interpolator
 
-class BESS_Trader:
+matplotlib.use('Agg')
+# Get BESS Data from Excel Dataframe
 
-   #Get BESS Data from Excel Dataframe
+file_path = "BESS Data.xlsx"
+file_path2 = "PUN.xlsx"
+sheetname = "BESS Properties"
+sheetname2 = "Li-ion ChargeDischarge Curve"
+Properties = ExcelOpener.import_file.load_excel(file_path, sheet_name=sheetname)
+load_curve = ExcelOpener.import_file.load_excel((file_path, sheet_name=sheetname2)
+PUN = ExcelOpener.import_file.load_excel(file_path2,
 
-   file_path = "BESS Data.xlsx"
-   sheetname = "BESS Properties"
-   sheetname2 = "Li-ion ChargeDischarge Curve"
-   Properties = ExcelOpener.import_file.load_excel(file_path, sheetname)
-   load_curve = ExcelOpener.import_file.load_excel(file_path, sheetname2)
+# Select charge_rate and discharge_rate DataFrames from load_curve
 
+charge_rate = load_curve.iloc[:356, [0, 3]]
+discharge_rate = load_curve.iloc[357:, [0, 4]]
 
+# Interpolate data for charge_rate and discharge_rate
+charge_interpolator = Interpolator.DataInterpolator(charge_rate, 'SoC [%]', 'Charge Rate [kWh/(kWhp*h)]')
+charge_rate_interpolated_func = charge_interpolator.interpolate()
+
+discharge_interpolator = Interpolator.DataInterpolator(discharge_rate, 'SoC [%]', 'Discharge Rate [kWh/(kWhp*h)]')
+discharge_rate_interpolated_func = discharge_interpolator.interpolate()
+
+# Plotting
+plt.figure(figsize=(10, 6))
+
+# Plot for charge_rate
+plt.plot(charge_rate['SoC [%]'], charge_rate['Charge Rate [kWh/(kWhp*h)]'], 'o', label='Charge Rate')
+plt.plot(charge_rate['SoC [%]'], charge_rate_interpolated_func(charge_rate['SoC [%]']), '-',
+label='Interpolated Charge Rate')
+
+# Plot for discharge_rate
+
+plt.plot(discharge_rate['SoC [%]'], discharge_rate['Discharge Rate [kWh/(kWhp*h)]'], 'o', color='red',
+             label='Discharge Rate')
+plt.plot(discharge_rate['SoC [%]'], discharge_rate_interpolated_func(discharge_rate['SoC [%]']), '-', color='green',
+             label='Interpolated Discharge rate')
+
+plt.xlabel('SoC [%]')
+plt.ylabel('Rate [kWh/(kWhp*h)]')
+plt.title('Interpolated Functions')
+plt.legend()
+plt.grid(True)
+
+# Save the plot as a PNG file
+
+if not os.path.exists("Plots"):
+    os.makedirs("Plots")
+plt.savefig("Plots/interpolated_functions.png")
+
+# Close the figure to release memory
+
+plt.close()
+
+# Plotting charge_rate
+
+plt.figure(figsize=(10, 6))
+plt.plot(charge_rate['SoC [%]'], charge_rate['Charge Rate [kWh/(kWhp*h)]'], label='Charge Rate')
+plt.xlabel('SoC [%]')
+plt.ylabel('Charge Rate [kWh/(kWhp*h)]')
+plt.title('Charge Rate vs SoC')
+plt.legend()
+plt.grid(True)
+
+# Save the plot as a PNG file
+
+plt.savefig("Plots/charge_rate_plot.png")
+plt.close()
+
+# Plotting discharge_rate
+
+plt.figure(figsize=(10, 6))
+plt.plot(discharge_rate['SoC [%]'], discharge_rate['Discharge Rate [kWh/(kWhp*h)]'], color='red',
+            label='Discharge Rate')
+plt.xlabel('SoC [%]')
+plt.ylabel('Discharge Rate [kWh/(kWhp*h)]')
+plt.title('Discharge Rate vs SoC')
+plt.legend()
+plt.grid(True)
+
+# Save the plot as a PNG file
+
+plt.savefig("Plots/discharge_rate_plot.png")
+plt.close()
 
 
 
---
+++ Interpolator.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import ExcelOpener
+import matplotlib.pyplot as plt
+import  matplotlib
+import os
+from scipy.interpolate import interp1d
+
+
+class DataInterpolator:
+    def __init__(self, df, x_col, y_col):
+        self.df = df
+        self.x_col = x_col
+        self.y_col = y_col
+
+    def interpolate(self, kind='linear'):
+        f = interp1d(self.df[self.x_col], self.df[self.y_col], kind=kind)
+        return f

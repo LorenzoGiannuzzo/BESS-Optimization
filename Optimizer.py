65dfd7d312829a054e14b21c6478d755ca8525f4
LorenzoGiannuzzo <lorenzo.giannuzzo@polito.it>
2024-06-13 00:14:04 +0200

    Update #12

--- Optimizer.py
+++ Optimizer.py
@@ -1,24 +1,25 @@
-import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
 import numpy as np
+
 from pymoo.algorithms.moo.nsga3 import NSGA3
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+from pymoo.operators.selection.tournament import TournamentSelection, compare
 from pymoo.optimize import minimize
 from pymoo.util.ref_dirs import get_reference_directions
-from pymoo.operators.sampling.rnd import FloatRandomSampling
+
+from BESS_parameters import charge_rate_interpolated_func, discharge_rate_interpolated_func
+from BESS_parameters import size, file_path2, sheetname3
+
+from Economic_parameters import PUN_timeseries
+
 from objective_function import Revenues
 from objective_function import pop_size
 from objective_function import termination
-from Economic_parameters import PUN_timeseries
-from BESS_parameters import charge_rate_interpolated_func, discharge_rate_interpolated_func
-from pymoo.operators.crossover.sbx import SBX
-from pymoo.operators.mutation.pm import PM
-from pymoo.operators.selection.tournament import TournamentSelection, compare
-from BESS_parameters import size, file_path, file_path2, sheetname, sheetname2, sheetname3
 from objective_function import time_window
-
-
-
-
+from objective_function import soc_0
 
 
 def comp_by_cv_then_random(pop, P, **kwargs):
@@ -38,26 +39,71 @@ def comp_by_cv_then_random(pop, P, **kwargs):
     return S[:, None].astype(int)
 
 
+# OPTIMIZER DEFINITION
+
 class Optimizer:
 
     def __init__(self, objective_function: Revenues, pop_size: pop_size):
         self._objective_function = objective_function
         self.pop_size = pop_size
 
+    # ALGORITHM DEFINITION
 
     def maximize_revenues(self):
-        ref_dirs = get_reference_directions("das-dennis", 1,n_partitions=20) # n_partitions=48
 
-        # Creare l'algoritmo con gli operatori definiti
+        # DEFINE REFERENCE DIRECTION
+
+        '''
+
+        Most studies have used the Das and Dennis’s structured approach for generating well-spaced reference points.
+        A reference direction is constructed by a vector originating from the origin and connected to each of them. The number
+        of points on the unit simplex is determined by a parameter p (we call it n_partitions in our implementation), which
+        indicates the number of gaps between two consecutive points along an objective axis.
+
+        '''
+
+        ref_dirs = get_reference_directions("das-dennis", 1, n_partitions=20)
+
+        # ALGORITHM AND HYPERPARAMETERS DEFINITION
+
         algorithm = NSGA3(
+
             pop_size=self.pop_size,
             ref_dirs=ref_dirs,
+
+            # sampling: This parameter specifies the method used to initialize the population. FloatRandomSampling
+            # generates random floating-point values for the initial solutions, providing a diverse starting point.
+
             sampling=FloatRandomSampling(),
+
+            # selection: This defines the selection mechanism used to choose parents for reproduction.
+            # TournamentSelection selects individuals based on a comparison function.
+            # func_comp=comp_by_cv_then_random: This comparison function first considers constraint violations (cv) and
+            # then applies a random selection if necessary. This helps prioritize feasible solutions.
+
             selection=TournamentSelection(func_comp=comp_by_cv_then_random),
-            eliminate_duplicates=True,
+
+            # crossover: This parameter specifies the crossover operator used for generating offspring.
+            # SBX: Simulated Binary Crossover (SBX) is a common crossover method in genetic algorithms, particularly
+            # suited for real-valued variables.
+            # eta=30: The distribution index for SBX. A higher value of eta results in offspring closer to their
+            # parents, while a lower value results in more variation.
+            # prob=1.0: The probability of crossover being applied. A probability of 1.0 means crossover is always
+            # applied.
+
             crossover=SBX(eta=30, prob=1.0),
+
+            # mutation: This parameter specifies the mutation operator used for generating variation in offspring.
+            # PM: Polynomial Mutation (PM) is a common mutation method for real-valued variables.
+            # eta=20: The distribution index for PM. Similar to SBX, a higher value of eta results in smaller mutations,
+            # while a lower value results in larger mutations.
+
             mutation=PM(eta=20),
+
+            eliminate_duplicates=True
         )
+
+        # INITIALIZE THE RESOLUTION OF THE OPTIMIZATION PROBLEM
 
         res = minimize(
             self._objective_function,
@@ -69,7 +115,8 @@ class Optimizer:
 
         return res
 
-# [OPTIMIZATION]
+# to do: parallelize the optimization problem resolution
+
 # import multiprocessing
 # from pymoo.algorithms.soo.nonconvex.ga import GA
 # from pymoo.core.problem import StarmapParallelization
@@ -78,31 +125,41 @@ class Optimizer:
 # n_proccess = 2
 # pool = multiprocessing.Pool(n_proccess)
 # runner = StarmapParallelization(pool.starmap)
+# ...
+# ...
+#  pool.close()
 
 
-objective_function = Revenues(size=size, pop_size=pop_size, file_path2=file_path2, sheetname3=sheetname3) #elementwise_runner=runner)
+# STATEMENT OF THE OBJECTIVE FUNCTION
 
-# Create an instance of Optimizer with the objective_function and pop_size
+objective_function = Revenues(size=size, pop_size=pop_size, file_path2=file_path2,
+                              sheetname3=sheetname3)  # elementwise_runner=runner)
+
+
+# LAUNCH THE OPTIMIZER
+
 optimizer = Optimizer(objective_function=objective_function, pop_size=pop_size)
 
-# Maximize revenues
-solution = optimizer.maximize_revenues()
-#pool.close()
+# GET SOLUTION OF THE OPTIMIZATION PROBLEM
 
-soc = [0.0] * 48
-charged_energy = [0.0]*48
-discharged_energy= [0.0]*48
+solution = optimizer.maximize_revenues()
+
+# GET THE BEST X-ARRAY FROM SOLUTIONS AND RE-APPLY THE PHYSICAL CONSTRAINTS OF THE BESS
 c_d_timeseries = solution.X
-soc[0] = 0.20
+
+soc = [0.0] * time_window
+charged_energy = [0.0] * time_window
+discharged_energy = [0.0] * time_window
+soc[0] = soc_0
 
 c_func = charge_rate_interpolated_func
 d_func = discharge_rate_interpolated_func
 
 for index in range(48 - 1):
     if c_d_timeseries[index] >= 0:
-        c_d_timeseries[index] = min(c_d_timeseries[index], min(c_func(soc[index]), 0.9-soc[index]))
+        c_d_timeseries[index] = min(c_d_timeseries[index], min(c_func(soc[index]), 0.9 - soc[index]))
     else:
-        c_d_timeseries[index] = max(c_d_timeseries[index], max(-d_func(soc[index]), -soc[index]+0.1))
+        c_d_timeseries[index] = max(c_d_timeseries[index], max(-d_func(soc[index]), -soc[index] + 0.1))
 
     if c_d_timeseries[index] >= 0:
         charged_energy[index] = c_d_timeseries[index] * size
@@ -111,16 +168,21 @@ for index in range(48 - 1):
 
         # UPDATE SoC
     if c_d_timeseries[index] >= 0:
-        soc[index + 1] = min(0.9, soc[index] + charged_energy[index]/ size)
+        soc[index + 1] = min(0.9, soc[index] + charged_energy[index] / size)
     else:
-        soc[index + 1] = max(0.1, soc[index] + discharged_energy[index]/ size)
+        soc[index + 1] = max(0.1, soc[index] + discharged_energy[index] / size)
 
 PUN_timeseries = PUN_timeseries
 
-rev = - (discharged_energy * PUN_timeseries/1000) - (charged_energy*PUN_timeseries/1000)
-print("Revenus for optimized time window:\n", rev.sum())
+rev = - (discharged_energy * PUN_timeseries / 1000) - (charged_energy * PUN_timeseries / 1000)
+print("Revenus for optimized time window [Euros]:\n", rev.sum())
+
+
+
+
 
 import matplotlib
+
 matplotlib.use('Agg')
 
 # Creazione degli istogrammi
@@ -134,7 +196,6 @@ plt.xlabel('Time step')
 plt.ylabel('SoC')
 plt.savefig(os.path.join("Plots", "SoC_hist.png"))
 plt.close()
-
 
 # Istogramma di charged energy
 plt.figure(figsize=(12, 8))
@@ -153,8 +214,6 @@ plt.xlabel('Time step')
 plt.ylabel('Discharged Energy')
 plt.savefig(os.path.join("Plots", "Discharged_Energy_hist.png"))
 plt.close()
-
-
 
 num_values = time_window
 time_steps_24 = time_steps[:num_values]
@@ -181,5 +240,3 @@ ax2.legend(loc='upper right')
 fig.tight_layout()
 plt.savefig(os.path.join("Plots", "Charged_and_Discharged_Energy_with_PUN_24.png"))
 plt.close()
-
-
--- utils.py
+++ utils.py
@@ -4,32 +4,43 @@ import Interpolator
 class Get_data:
     @staticmethod
     def get_data(file_path2, sheetname3):
+        # Import data from an Excel file and a specific sheet using a method from the ExcelOpener module.
         data = ExcelOpener.import_file.load_excel(file_path2, sheetname3)
+        # Return the imported data.
         return data
 
 class BESS:
 
     @staticmethod
     def get_bess(technology, properties, se_sp, size):
+        # Select specific columns from the properties DataFrame.
         BESS_Parameters = properties.iloc[:, [0, 1, 7, 9, 14, 15]]
+        # Filter the DataFrame for the specified technology.
         BESS_Parameters = BESS_Parameters[BESS_Parameters['Technology'] == technology]
+        # Further filter the DataFrame for the specified specific energy/specific power.
         BESS_Parameters = BESS_Parameters[BESS_Parameters['Specific Energy / Specific Power'] == se_sp]
+        # Add a new column for the size in kWh.
         BESS_Parameters['Size [kWh]'] = size
+        # Select specific columns to keep in the final DataFrame.
         BESS_Parameters = BESS_Parameters.iloc[:, [0, 1, 2, 3, 6]]
 
+        # Return the filtered and modified DataFrame.
         return BESS_Parameters
 
     @staticmethod
     def get_c_d_functions(load_curve):
-        # Select charge_rate and discharge_rate DataFrames from load_curve
+        # Select charge rate DataFrame from the first 356 rows of load_curve.
         charge_rate = load_curve.iloc[:356, [0, 3.5]]
+        # Select discharge rate DataFrame from rows 357 onwards of load_curve.
         discharge_rate = load_curve.iloc[357:, [0, 4, 5]]
-        # Interpolate data for charge_rate and discharge_rate
+        # Interpolate data for the charge rate.
         charge_interpolator = Interpolator.DataInterpolator(charge_rate, 'SoC [%]', 'Charge Rate [kWh/(kWhp*h)]')
         charge_rate_interpolated_func = charge_interpolator.interpolate()
-        discharge_interpolator = Interpolator.DataInterpolator(discharge_rate, 'SoC [%]',
-                                                               'Discharge Rate [kWh/(kWhp*h)]')
+        # Interpolate data for the discharge rate.
+        discharge_interpolator = Interpolator.DataInterpolator(discharge_rate, 'SoC [%]', 'Discharge Rate [kWh/(kWhp*h)]')
         discharge_rate_interpolated_func = discharge_interpolator.interpolate()
 
+        # Return the interpolated functions for charge and discharge rates.
         return charge_rate_interpolated_func, discharge_rate_interpolated_func
+
 

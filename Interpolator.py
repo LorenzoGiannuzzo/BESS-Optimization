d1a482d90855d50f89ba6a6825347870a16ebfa7
LorenzoGiannuzzo <lorenzo.giannuzzo@polito.it>
2024-11-06 17:40:15 +0100

    Code Review of Interpolator.py

--- Interpolator.py
+++ Interpolator.py
@@ -1,10 +1,34 @@
+"""
+
+BESS Optimization using NSGA-III Algorithm
+
+    __author__ = "Lorenzo Giannuzzo"
+    __maintainer__ = "Lorenzo Giannuzzo"
+    __email__ = "lorenzo.giannuzzo@polito.it"
+    __status__ = "in progress"
+    __version__ = "v0.2.1"
+    __license__ = "MIT"
+
+Last Update of current code: 06/11/2024 - 17:39
+
+"""
+
+# IMPORT LIBRARIES
+
 from scipy.interpolate import interp1d
+
+# CREATE DATAINTERPOLATOR CLASS
+
 class DataInterpolator:
+
     def __init__(self, df, x_col, y_col):
+
         self.df = df
         self.x_col = x_col
         self.y_col = y_col
 
     def interpolate(self, kind='linear'):
+
         f = interp1d(self.df[self.x_col], self.df[self.y_col], kind=kind)
+
         return f

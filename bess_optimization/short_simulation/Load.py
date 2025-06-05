c8e28a003ef0dcf2145d323ea927300375960ec0
LorenzoGiannuzzo <lorenzo.giannuzzo@polito.it>
2025-06-05 19:52:13 +0200

    Fixed default input settings

--- bess_optimization/short_simulation/Load.py
+++ bess_optimization/short_simulation/Load.py
@@ -1,44 +1,56 @@
 # IMPORTING REQUIRED LIBRARIES
 from argparser_s import input_load, rec_load  # Importing input load parameter
+from Economic_parameters_s import time_window
 import ExcelOpener_s  # Module for opening Excel files
 import pandas as pd  # Data manipulation and analysis
 import numpy as np
 
-# LOAD EXCEL FILE AND SPECIFIC SHEET
-df = ExcelOpener_s.import_file.load_excel(input_load, 'Sheet 1')
+if input_load != 0.0:
 
-# CONVERT 'Data' COLUMN TO DATETIME FORMAT
-df['Data'] = pd.to_datetime(df['Data'])
+    # LOAD EXCEL FILE AND SPECIFIC SHEET
+    df = ExcelOpener_s.import_file.load_excel(input_load, 'Sheet 1')
 
-# FILTER DATAFRAME FOR ROWS CORRESPONDING TO THE 15TH DAY OF EACH MONTH
-df_15 = df[df['Data'].dt.day == 15]
+    # CONVERT 'Data' COLUMN TO DATETIME FORMAT
+    df['Data'] = pd.to_datetime(df['Data'])
 
-# EXTRACT 'value' COLUMN FROM FILTERED DATAFRAME
-values_15 = df_15['value']
+    # FILTER DATAFRAME FOR ROWS CORRESPONDING TO THE 15TH DAY OF EACH MONTH
+    df_15 = df[df['Data'].dt.day == 15]
 
-# CONVERT DATAFRAME TO NUMPY ARRAY AND EXTRACT SPECIFIC COLUMN
-data = df_15.to_numpy()  # Convert DataFrame to NumPy array
-data = data[:, 4]  # Extract the 5th column (index 4) from the array
+    # EXTRACT 'value' COLUMN FROM FILTERED DATAFRAME
+    values_15 = df_15['value']
 
-# IN CASE YOU WANT TO SIMULATE ONLY PURE TRADING
-#data = np.zeros(len(data))
+    # CONVERT DATAFRAME TO NUMPY ARRAY AND EXTRACT SPECIFIC COLUMN
+    data = df_15.to_numpy()  # Convert DataFrame to NumPy array
+    data = data[:, 4]  # Extract the 5th column (index 4) from the array
 
-df_rec = ExcelOpener_s.import_file.load_excel(rec_load, 'Sheet 1')
+    # IN CASE YOU WANT TO SIMULATE ONLY PURE TRADING
+    #data = np.zeros(len(data))
 
-# CONVERT 'Data' COLUMN TO DATETIME FORMAT
-df_rec['Data'] = pd.to_datetime(df_rec['Data'])
+else:
 
-# FILTER DATAFRAME FOR ROWS CORRESPONDING TO THE 15TH DAY OF EACH MONTH
-df_15_rec = df_rec[df_rec['Data'].dt.day == 15]
+    data = np.zeros(time_window)
 
-# EXTRACT 'value' COLUMN FROM FILTERED DATAFRAME
-values_15_rec = df_15_rec['value']
 
-# CONVERT DATAFRAME TO NUMPY ARRAY AND EXTRACT SPECIFIC COLUMN
-data_rec = df_15_rec.to_numpy()  # Convert DataFrame to NumPy array
-data_rec = data_rec[:, 4]  # Extract the 5th column (index 4) from the array
+if rec_load != 0.0:
+    df_rec = ExcelOpener_s.import_file.load_excel(rec_load, 'Sheet 1')
 
-data_rec = pd.to_numeric(data_rec)
+    # CONVERT 'Data' COLUMN TO DATETIME FORMAT
+    df_rec['Data'] = pd.to_datetime(df_rec['Data'])
 
-data_rec = np.array(data_rec)
+    # FILTER DATAFRAME FOR ROWS CORRESPONDING TO THE 15TH DAY OF EACH MONTH
+    df_15_rec = df_rec[df_rec['Data'].dt.day == 15]
 
+    # EXTRACT 'value' COLUMN FROM FILTERED DATAFRAME
+    values_15_rec = df_15_rec['value']
+
+    # CONVERT DATAFRAME TO NUMPY ARRAY AND EXTRACT SPECIFIC COLUMN
+    data_rec = df_15_rec.to_numpy()  # Convert DataFrame to NumPy array
+    data_rec = data_rec[:, 4]  # Extract the 5th column (index 4) from the array
+
+    data_rec = pd.to_numeric(data_rec)
+
+    data_rec = np.array(data_rec)
+
+else:
+
+    data_rec = np.zeros(time_window)
